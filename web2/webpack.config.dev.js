42f7ff4b5f76f5504bf0db713e9ee86419b2011f
seryunkim <sr0217@gmail.com>
2018-02-20 19:02:25 +0900

    web

---
+++ web2/webpack.config.dev.js
@@ -0,0 +1,32 @@
+// webpack.config.dev.js
+var path = require('path')
+var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  devtool: 'cheap-eval-source-map',
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.css$/,
+      loaders: ['style', 'css']
+    }]
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
+}
